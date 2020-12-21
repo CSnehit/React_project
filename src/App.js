fafec3252e754f6c0d7d08cc3e409e23bd1c8e99
Snehit <csnehit@gmail.com>
2020-12-22 01:13:54 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+//Import components
+import Carddex from "./components/Carddex";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Carddex/>
+    </div>
+  );
+}
+
+export default App;
